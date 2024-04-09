b06c538156a5a4c328cea5469667f45e409a5648
brylka <brylka@jelenia.com>
2024-04-09 09:34:00 +0200

    adding the names of the days of the week

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+	// tablica z dniami tygodnia
+	var dayName = ["Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie"];
+	
+	// wyświetlenie dni tygodnia
+	for (let i = 0; i <= 6; i++) {
+		$("#calendar").append('<div class="day-name">' + dayName[i] + '</div>');
+	}
+	
+	// wyświetlenie kalendarza
 	for (let i = 1; i <= 31; i++) {
 		//if (i % 7 != 1) {
 		//	$("#calendar").append("<div>" + i + "</div>")
@@ -6,7 +15,5 @@ $(document).ready(function() {
 		//	$("#calendar").append('<div class="clear">' + i + '</div>');
 		//}
 		$("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : '') + '">' + i + '</div>')
-		
-		
 	}
 })
