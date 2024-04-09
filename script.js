ef677b5475f97a488c4e1b41f98c4f61625c7d8f
brylka <brylka@jelenia.com>
2024-04-09 09:24:35 +0200

    converting the condition into a ternary operator

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 	for (let i = 1; i <= 31; i++) {
-		if (i % 7 != 1) {
-			$("#calendar").append("<div>" + i + "</div>")
-		} else {
-			$("#calendar").append('<div class="clear">' + i + '</div>');
-		}
+		//if (i % 7 != 1) {
+		//	$("#calendar").append("<div>" + i + "</div>")
+		//} else {
+		//	$("#calendar").append('<div class="clear">' + i + '</div>');
+		//}
+		$("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : '') + '">' + i + '</div>')
+		
+		
 	}
 })
