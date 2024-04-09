fee5fc3a8afdea40cdfff1755aa6d7c08b14a154
brylka <brylka@jelenia.com>
2024-04-09 09:18:28 +0200

    adding a transition to a new line after Sunday

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	for (let i = 1; i <= 31; i++) {
-		$("#calendar").append("<div>" + i + "</div>")
+		if (i % 7 != 1) {
+			$("#calendar").append("<div>" + i + "</div>")
+		} else {
+			$("#calendar").append('<div class="clear">' + i + '</div>');
+		}
 	}
 })
