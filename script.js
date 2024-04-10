05b20b3513960a78fc6e0e440a0d40b713834bd8
brylka <brylka@jelenia.com>
2024-04-10 08:43:19 +0200

    refactoring replacing the ternary operator with addClass

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ $(document).ready(function() {
 	
 	// wyświetlenie dni tygodnia
 	for (let i = 0; i <= 6; i++) {
-		$("#calendar").append('<div class="day-name' +
-		(i == 5 ? ' saturday' : i == 6 ? ' sunday' : '') 
-		+ '">' + dayName[i] + '</div>');
+		//$("#calendar").append('<div class="day-name' + (i == 5 ? ' saturday' : i == 6 ? ' sunday' : '') + '">' + dayName[i] + '</div>');
+		divDay = $('<div>').text(dayName[i]);
+		divDay.addClass('day-name');
+		if (i == 5) { divDay.addClass('saturday'); }
+		if (i == 6) { divDay.addClass('sunday'); }
+		$("#calendar").append(divDay);
 	}
 	
 	// wyświetlenie kalendarza
 	for (let i = 1; i <= 31; i++) {
 		// wyświetlanie dni miesiąca
-		$("#calendar").append('<div class="' +
-		((i % 7 == 1) ? 'clear' : (i % 7 == 0) ? 'sunday' : (i % 7 == 6) ? 'saturday' : '') + 
-		'">' + i + '</div>')
+		//$("#calendar").append('<div class="' +
+		//((i % 7 == 1) ? 'clear' : (i % 7 == 0) ? 'sunday' : (i % 7 == 6) ? 'saturday' : '') + 
+		//'">' + i + '</div>')
+		divDay = $('<div>').text(i);
+		if (i % 7 == 1) { divDay.addClass('clear'); }
+		if (i % 7 == 0) { divDay.addClass('sunday'); }
+		if (i % 7 == 6) { divDay.addClass('saturday'); }
+		$("#calendar").append(divDay);
+		
 	}
 })
